7b5622c834b765bd995e481473fd12f4c82701b1
Krzysztof Mierzejewski <krzysiek@flsot.home>
2018-03-10 16:48:25 +0100

    Fix review pull requests

--- app/models/dormitory.rb
+++ app/models/dormitory.rb
@@ -1,9 +1,3 @@
 class Dormitory < ApplicationRecord
-    attr_accessor :id, :name, :address
-
-    def initialize(id, name, address)
-        @id = id
-        @name = name
-        @address = address
-    end
+    
 end
