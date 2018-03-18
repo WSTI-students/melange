d64531cc8d758c1a0b747fc21576dda76e4e5e2b
Adam <adalit@gmail.com>
2018-03-18 12:37:12 +0100

    Event-user connection and displaying event details

--- db/migrate/20180311110608_create_gifts.rb
+++ db/migrate/20180311110608_create_gifts.rb
@@ -1,10 +1,10 @@
 class CreateGifts < ActiveRecord::Migration[5.0]
   def change
     create_table :gifts do |t|
-      t.string :name, 
-      t.integer :count,
-      t.references :users,
-      t.references :events,
+      t.string :name
+      t.integer :count
+      t.references :users
+      t.references :events
       t.timestamps
     end
   end
